5f42841a323da9b7e34f035fd62b73ae94806808
bwzheng2010 <bwzheng2010@gmail.com>
2025-04-22 22:40:18 +0800

    dev statement

--- utils/const.py
+++ utils/const.py
@@ -16,3 +16,9 @@ daily_treasury_yield="daily_treasury_yield"
 tables = [stock_profile, stock_officers, stock_summary, stock_tailing_eps, stock_earning_calendar, stock_revenue_estimates, stock_earning_estimates,
                 stock_historical_eps, stock_statement, stock_prices, stock_dividend_events, stock_split_events, exchange_rate, daily_treasury_yield]
 
+income_statement = "income_statement"
+balance_sheet = "balance_sheet"
+cash_flow = "cash_flow"
+
+quarterly = "quarterly"
+annual = "annual"
