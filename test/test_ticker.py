8ed1ad495d7ef71bc6e27836225bc5205b8ef5f2
zxh2010 <5077921+zxh2010@users.noreply.github.com>
2025-09-03 00:12:40 +0800

    fix #40 Fix YoY calculation for discontinuous quarters

--- test/test_ticker.py
+++ test/test_ticker.py
@@ -7,7 +7,7 @@ from defeatbeta_api.data.ticker import Ticker
 class TestTicker(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.ticker = Ticker("TSLA", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
+        cls.ticker = Ticker("AMD", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
 
     @classmethod
     def tearDownClass(cls):
@@ -216,4 +216,4 @@ class TestTicker(unittest.TestCase):
 
     def test_peg_ratio(self):
         result = self.ticker.peg_ratio()
-        print(result)
+        print(result.to_string())
