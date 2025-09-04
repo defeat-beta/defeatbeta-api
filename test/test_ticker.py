ed6d52c7bea62d472b388312d7802e9fd8e8dc7e
bwzheng2010 <5077921+bwzheng2010@users.noreply.github.com>
2025-09-04 10:11:35 +0800

    update test case

--- test/test_ticker.py
+++ test/test_ticker.py
@@ -7,7 +7,7 @@ from defeatbeta_api.data.ticker import Ticker
 class TestTicker(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.ticker = Ticker("LI", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
+        cls.ticker = Ticker("NVDA", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
 
     @classmethod
     def tearDownClass(cls):
@@ -220,4 +220,8 @@ class TestTicker(unittest.TestCase):
 
     def test_ttm_revenue(self):
         result = self.ticker._ttm_revenue()
+        print(result.to_string())
+
+    def test_quarterly_book_value_of_equity(self):
+        result = self.ticker._quarterly_book_value_of_equity()
         print(result.to_string())
