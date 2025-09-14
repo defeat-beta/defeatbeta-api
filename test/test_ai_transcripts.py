572e8d105efd1d58a885f1a1abde6a5fc0ed6459
bwzheng2010 <bwzheng2010@gmail.com>
2025-09-14 23:32:46 +0800

    fix #49 AI-Powered Earnings Call Transcripts - Optimize default llm's temperature

--- test/test_ai_transcripts.py
+++ test/test_ai_transcripts.py
@@ -9,7 +9,7 @@ from openai import OpenAI
 class TestAITranscripts(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.ticker = Ticker("AMD", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
+        cls.ticker = Ticker("NVDA", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
 
         key = Path(__file__).parent.joinpath("siliconflow_api.key").read_text(encoding="utf-8")
         cls.llm = OpenAI(
@@ -24,5 +24,9 @@ class TestAITranscripts(unittest.TestCase):
 
     def test_summarize_key_financial_data_with_ai(self):
         transcripts = self.ticker.earning_call_transcripts()
-        res = transcripts.summarize_key_financial_data_with_ai(2025, 2, self.llm, OpenAIConfiguration())
+        res = transcripts.summarize_key_financial_data_with_ai(
+            2026,
+            2,
+            self.llm,
+            OpenAIConfiguration(model='Qwen/Qwen3-8B', temperature=0))
         print(res.to_string())
