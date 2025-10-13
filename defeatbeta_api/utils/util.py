2d29aa909730a4e3f66059433b73d0c08694f083
bwzheng2010 <bwzheng2010@gmail.com>
2025-10-13 22:18:14 +0800

    refactor

--- defeatbeta_api/utils/util.py
+++ defeatbeta_api/utils/util.py
@@ -135,7 +135,7 @@ def load_transcripts_summary_prompt_temp() -> str:
     return text
 
 def load_transcripts_analyze_up_and_down_prompt() -> str:
-    text = files("defeatbeta_api.data.template").joinpath('transcripts_analyze_financial_metrics_change_tools.md').read_text(encoding="utf-8")
+    text = files("defeatbeta_api.data.template").joinpath('transcripts_analyze_financial_metrics_change_prompt.md').read_text(encoding="utf-8")
     return text
 
 def load_transcripts_summary_tools_def() -> Dict[str, Any]:
