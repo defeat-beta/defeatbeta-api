2c511b564205cf2569cf082103b141035b293c6c
bwzheng2010 <5077921+bwzheng2010@users.noreply.github.com>
2025-10-22 21:13:07 +0800

    update readme

--- test/test_ai_transcripts.py
+++ test/test_ai_transcripts.py
@@ -9,7 +9,7 @@ from openai import OpenAI
 class TestAITranscripts(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.ticker = Ticker("JPM", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
+        cls.ticker = Ticker("NFLX", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
 
         key = Path(__file__).parent.joinpath("siliconflow_api.key").read_text(encoding="utf-8")
         cls.llm = OpenAI(
@@ -35,7 +35,7 @@ class TestAITranscripts(unittest.TestCase):
         transcripts = self.ticker.earning_call_transcripts()
         res = transcripts.analyze_financial_metrics_change_for_this_quarter_with_ai(
             2025,
-            3,
+            2,
             self.llm,
             OpenAIConfiguration(model='Qwen/Qwen3-Omni-30B-A3B-Thinking', temperature=0, top_p=0, tool_choice="auto"))
         print(res.to_string())
@@ -43,8 +43,8 @@ class TestAITranscripts(unittest.TestCase):
     def test_analyze_financial_metrics_forecast_for_future_with_ai(self):
         transcripts = self.ticker.earning_call_transcripts()
         res = transcripts.analyze_financial_metrics_forecast_for_future_with_ai(
-            2026,
-            1,
+            2025,
+            2,
             self.llm,
             OpenAIConfiguration(model='Qwen/Qwen3-Omni-30B-A3B-Thinking', temperature=0, top_p=0, tool_choice="auto"))
         print(res.to_string())
