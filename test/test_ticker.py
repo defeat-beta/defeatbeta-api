5f9dc56c897ccd910402565229de2c91331ac272
bwzheng2010 <5077921+bwzheng2010@users.noreply.github.com>
2025-11-11 17:24:34 +0800

    fix #88 Optimize the perf of Industry PE

--- test/test_ticker.py
+++ test/test_ticker.py
@@ -7,7 +7,7 @@ from defeatbeta_api.data.ticker import Ticker
 class TestTicker(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.ticker = Ticker("BABA", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
+        cls.ticker = Ticker("PDD", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
 
     @classmethod
     def tearDownClass(cls):
