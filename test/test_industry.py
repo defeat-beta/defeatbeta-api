459765e0216f93a23e1f2223e70f2267db3a9aa9
bwzheng2010 <5077921+bwzheng2010@users.noreply.github.com>
2025-11-18 18:22:01 +0800

    fix #65 Support Industry Assert Turnover Ratio

--- test/test_industry.py
+++ test/test_industry.py
@@ -6,7 +6,7 @@ from defeatbeta_api.data.ticker import Ticker
 class TestTicker(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-            cls.ticker = Ticker("LI", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
+            cls.ticker = Ticker("TSLA", http_proxy="http://127.0.0.1:33210", log_level=logging.DEBUG)
 
     @classmethod
     def tearDownClass(cls):
@@ -53,4 +53,10 @@ class TestTicker(unittest.TestCase):
         result = self.ticker.quarterly_net_margin()
         print(result.to_string())
         result = self.ticker.industry_quarterly_net_margin()
+        print(result.to_string())
+
+    def test_industry_asset_turnover(self):
+        result = self.ticker.quarterly_net_margin()
+        print(result.to_string())
+        result = self.ticker.industry_asset_turnover()
         print(result.to_string())
