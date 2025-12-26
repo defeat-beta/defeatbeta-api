b7ad5dfef79ddb1ec981f41a129ddacd86ceb3eb
bwzheng2010 <bwzheng2010@gmail.com>
2025-12-26 14:58:02 +0800

    to #116

--- mcp/src/defeatbeta_mcp/server.py
+++ mcp/src/defeatbeta_mcp/server.py
@@ -2,7 +2,7 @@ from mcp.server.fastmcp import FastMCP
 
 from .tools.officers import get_stock_officers
 from .tools.profile import get_stock_profile
-from .tools.meta import get_latest_data_update_date, get_defeatbeta_api_version
+from .tools.meta import get_latest_data_update_date, get_defeatbeta_api_version, get_current_datetime
 from .tools.price import get_stock_price
 from .tools.transcripts import get_stock_earning_call_transcripts_list, get_stock_earning_call_transcript
 
@@ -14,6 +14,7 @@ mcp = FastMCP(
 
 mcp.tool()(get_defeatbeta_api_version)
 mcp.tool()(get_latest_data_update_date)
+mcp.tool()(get_current_datetime)
 mcp.tool()(get_stock_profile)
 mcp.tool()(get_stock_price)
 mcp.tool()(get_stock_officers)
