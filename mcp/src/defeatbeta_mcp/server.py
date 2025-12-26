c0dc98866a4672094748316a6383fe446ea64453
zxh2010 <5077921+zxh2010@users.noreply.github.com>
2025-12-26 09:51:29 +0800

    #116

--- mcp/src/defeatbeta_mcp/server.py
+++ mcp/src/defeatbeta_mcp/server.py
@@ -2,8 +2,9 @@ from mcp.server.fastmcp import FastMCP
 
 from .tools.officers import get_stock_officers
 from .tools.profile import get_stock_profile
-from .tools.meta import get_latest_data_update_date
+from .tools.meta import get_latest_data_update_date, get_defeatbeta_api_version
 from .tools.price import get_stock_price
+from .tools.transcripts import get_stock_earning_call_transcripts_list, get_stock_earning_call_transcript
 
 mcp = FastMCP(
     name="Defeat Beta API",
@@ -11,16 +12,17 @@ mcp = FastMCP(
     website_url="https://github.com/defeat-beta/defeatbeta-api"
 )
 
+mcp.tool()(get_defeatbeta_api_version)
 mcp.tool()(get_latest_data_update_date)
 mcp.tool()(get_stock_profile)
 mcp.tool()(get_stock_price)
 mcp.tool()(get_stock_officers)
-
+mcp.tool()(get_stock_earning_call_transcripts_list)
+mcp.tool()(get_stock_earning_call_transcript)
 
 
 def main():
     mcp.run()
-
 
 if __name__ == "__main__":
     main()
